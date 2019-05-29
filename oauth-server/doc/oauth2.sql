e7b759e2057c4d2b7ee27b43fa4b2b42c7230036
baojunz <78911244@qq.com>
2019-05-29 18:09:04 +0800

    sql update

--- oauth-server/doc/oauth2.sql
+++ oauth-server/doc/oauth2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： db
--- 生成日期： 2019-05-29 09:31:14
+-- 生成日期： 2019-05-29 10:07:48
 -- 服务器版本： 5.6.44
 -- PHP 版本： 7.2.13
 
@@ -11,12 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- 数据库： `oauth2`
@@ -29,17 +23,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `clientdetails` (
-  `appId` varchar(255) NOT NULL,
-  `resourceIds` varchar(255) DEFAULT NULL,
-  `appSecret` varchar(255) DEFAULT NULL,
-  `scope` varchar(255) DEFAULT NULL,
-  `grantTypes` varchar(255) DEFAULT NULL,
-  `redirectUrl` varchar(255) DEFAULT NULL,
-  `authorities` varchar(255) DEFAULT NULL,
+  `appId` varchar(128) NOT NULL,
+  `resourceIds` varchar(256) DEFAULT NULL,
+  `appSecret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `grantTypes` varchar(256) DEFAULT NULL,
+  `redirectUrl` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
   `access_token_validity` int(11) DEFAULT NULL,
   `refresh_token_validity` int(11) DEFAULT NULL,
   `additionalInformation` varchar(4096) DEFAULT NULL,
-  `autoApproveScopes` varchar(255) DEFAULT NULL
+  `autoApproveScopes` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -49,13 +43,13 @@ CREATE TABLE `clientdetails` (
 --
 
 CREATE TABLE `oauth_access_token` (
-  `token_id` varchar(255) DEFAULT NULL,
+  `token_id` varchar(256) DEFAULT NULL,
   `token` blob,
-  `authentication_id` varchar(255) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `client_id` varchar(255) DEFAULT NULL,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
   `authentication` blob,
-  `refresh_token` varchar(255) DEFAULT NULL
+  `refresh_token` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -65,12 +59,12 @@ CREATE TABLE `oauth_access_token` (
 --
 
 CREATE TABLE `oauth_approvals` (
-  `userId` varchar(255) DEFAULT NULL,
-  `clientId` varchar(255) DEFAULT NULL,
-  `scope` varchar(255) DEFAULT NULL,
+  `userId` varchar(256) DEFAULT NULL,
+  `clientId` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
   `status` varchar(10) DEFAULT NULL,
-  `expiresAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `lastModifiedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+  `expiresAt` datetime DEFAULT NULL,
+  `lastModifiedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -80,17 +74,17 @@ CREATE TABLE `oauth_approvals` (
 --
 
 CREATE TABLE `oauth_client_details` (
-  `client_id` varchar(255) NOT NULL,
-  `resource_ids` varchar(255) DEFAULT NULL,
-  `client_secret` varchar(255) DEFAULT NULL,
-  `scope` varchar(255) DEFAULT NULL,
-  `authorized_grant_types` varchar(255) DEFAULT NULL,
-  `web_server_redirect_uri` varchar(255) DEFAULT NULL,
-  `authorities` varchar(255) DEFAULT NULL,
+  `client_id` varchar(128) NOT NULL,
+  `resource_ids` varchar(256) DEFAULT NULL,
+  `client_secret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `authorized_grant_types` varchar(256) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
   `access_token_validity` int(11) DEFAULT NULL,
   `refresh_token_validity` int(11) DEFAULT NULL,
   `additional_information` varchar(4096) DEFAULT NULL,
-  `autoapprove` varchar(255) DEFAULT NULL
+  `autoapprove` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -98,7 +92,7 @@ CREATE TABLE `oauth_client_details` (
 --
 
 INSERT INTO `oauth_client_details` (`client_id`, `resource_ids`, `client_secret`, `scope`, `authorized_grant_types`, `web_server_redirect_uri`, `authorities`, `access_token_validity`, `refresh_token_validity`, `additional_information`, `autoapprove`) VALUES
-('client', NULL, '$2a$10$M4NhY2X4anHW4X2v1dqJTuhe3tjbfOQiH3P9yfqWDvukZSeZfbpPu', 'all', 'authorization_code', 'http://www.baidu.com', NULL, NULL, NULL, NULL, NULL);
+(''client'', NULL, ''$2a$10$M4NhY2X4anHW4X2v1dqJTuhe3tjbfOQiH3P9yfqWDvukZSeZfbpPu'', ''all'', ''authorization_code'', ''http://www.baidu.com'', NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -107,11 +101,11 @@ INSERT INTO `oauth_client_details` (`client_id`, `resource_ids`, `client_secret`
 --
 
 CREATE TABLE `oauth_client_token` (
-  `token_id` varchar(255) DEFAULT NULL,
+  `token_id` varchar(256) DEFAULT NULL,
   `token` blob,
-  `authentication_id` varchar(255) NOT NULL,
-  `user_name` varchar(255) DEFAULT NULL,
-  `client_id` varchar(255) DEFAULT NULL
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -121,7 +115,7 @@ CREATE TABLE `oauth_client_token` (
 --
 
 CREATE TABLE `oauth_code` (
-  `code` varchar(255) DEFAULT NULL,
+  `code` varchar(256) DEFAULT NULL,
   `authentication` blob
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -132,7 +126,7 @@ CREATE TABLE `oauth_code` (
 --
 
 CREATE TABLE `oauth_refresh_token` (
-  `token_id` varchar(255) DEFAULT NULL,
+  `token_id` varchar(256) DEFAULT NULL,
   `token` blob,
   `authentication` blob
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -165,7 +159,3 @@ ALTER TABLE `oauth_client_details`
 ALTER TABLE `oauth_client_token`
   ADD PRIMARY KEY (`authentication_id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
